3e027a98bf71eccc6a1c0371e35b981f8d08f828
Aravind Sridhar <aravindsri30@gmail.com>
2023-12-25 17:28:17 +0530

    Updated app.py to generate responses from gemini-pro

--- app.py
+++ app.py
@@ -1,3 +1,31 @@
+import streamlit as st
+import os
+import google.generativeai as ga
+
 from dotenv import load_dotenv
+
 load_dotenv()
 
+ga.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+model = ga.GenerativeModel('gemini-pro')
+
+def get_gemini_pro(question):
+    response = model.generate_content(question)
+
+    return response.text
+
+
+st.set_page_config(
+    page_title="Gemini Pro Model Generator",
+)
+
+st.header('Gemini Pro Model Generator')
+
+ip = st.text_input("Input: ", key="input")
+submit = st.button("Submit Query")
+
+if submit:
+    op = get_gemini_pro(ip)
+    st.subheader("Gemini Pro Model Generator Output:")
+    st.write("Output: ", op)
